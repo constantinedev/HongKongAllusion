aea2fa5ab82c48c04471a51d3a4c9e4eab9a551c
Constantine Dev <1324252+constantinedev@users.noreply.github.com>
2022-03-02 06:43:19 +0000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,22 +1,21 @@
 import io, os, sys, json, requests, youtube_dl, shutil, time
 import PySimpleGUI as sg
-from pytube.streams import Stream
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem
 
-software_names = [SoftwareName.CHROME.value]
-operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]   
+software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value]
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.MAC.value, OperatingSystem.LINUX.value]
 user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
 user_agent = user_agent_rotator.get_random_user_agent()
 
 Apptitle = '香港長故'
 sg.theme('SystemDefault')
 
+payload = {}
 headers = {
     'User-Agent': user_agent,
     'Content-Type': 'application/json'
 }
-payload = {}
 
 session = requests.session()
 session.proxies = {}
@@ -45,57 +44,10 @@ class pre_options():
 layout = [
         [sg.Text('媒體'), sg.Combo(list(pre_options.offic_lst), enable_events=True, key='office', size=(40, 5))],
         [sg.Text('類型'), sg.Combo(['圖片', '影片'], key='opts', size=(15, 5)), sg.Button('全自動備份', key='start_btn'), sg.Button('停止', key='stop_btn')],
-        [sg.Output(size=(80, 20), key='res_dp')]
+        [sg.Output(size=(120, 10), key='res_dp', background_color='black', text_color="#00FF00", echo_stdout_stderr=False)]
     ]
 
 window = sg.Window(title=str(Apptitle), layout=layout)
-    
-def img_downloader(post_id, savdir, FileName, dw_url):
-    if FileName + '.jpg' in os.listdir(savdir):
-        print(str(post_id) + " => 已下載 - Downloaded")
-        pass
-    else:
-        if dw_url is not None:
-            req = requests.get(str(dw_url), stream=True, headers=headers, data=payload)
-            if req.status_code == 200:
-                try:
-                    with open(savdir + FileName + '.jpg', 'wb') as dw_img:
-                        shutil.copyfileobj(req.raw, dw_img)
-                        dw_img.close()
-                    print(str(post_id) + " => 完成 - Done" + str(req.status_code))
-                    pass
-                except:                
-                    with open(savdir + FileName, 'wb') as dw_img:
-                        shutil.copyfileobj(req.raw, dw_img)
-                        dw_img.close()
-                    print(str(post_id) + " => 完成 - Done " + str(req.status_code))
-                    pass
-            else:
-                print(str(post_id) + " => Error Code: " + str(req.status_code))
-                pass
-            req.close()
-        pass
-
-
-def video_downloader(post_id, sav_dir, FileName, dw_url):
-    dwn_url = str(dw_url).replace('https://www.youtube.com/embed/', 'https://www.youtube.com/watch?v=')
-    with youtube_dl.YoutubeDL(payload) as ydl:
-        meta_data = ydl.extract_info(dwn_url, download=False)
-        video_title = meta_data.get('title', None)
-    # json.dump(meta_data, sav_dir + post_id + '_' + FileName + '.json', ensure_ascii=False, indent=4)
-    
-    ydl_opts = {
-        'format': 'bestvideo/best',
-        'noplaylist': True,
-        'outtmpl': sav_dir + FileName + '_' + video_title + '.%(ext)s',
-    }
-    
-    # responser = requests.get(dwn_url, headers=headers, data=payload)
-    # ts = responser.status_code()
-    # responser.close()
-        
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([dwn_url])
 
 def main():
     while True:
@@ -103,6 +55,7 @@ def main():
         if events == sg.WIN_CLOSED:
             break
         if events == 'stop_btn':
+            window['res_dp'].update(values='')
             break
         
         if events == 'office':
@@ -114,7 +67,7 @@ def main():
             offic = pre_options.offic_lst[str(values['office'])]
             opt = pre_options.opts_lst[str(values['office'])][str(values['opts'])]
             
-            sav_dir = 'resultes/' + str(offic) + '/' + str(opt) + '/'            
+            sav_dir = pre_options.DataDir + '/' + str(offic) + '/' + str(opt) + '/'
             if not os.path.exists(sav_dir):
                 os.makedirs(sav_dir)
                 pass
@@ -125,7 +78,8 @@ def main():
                 SELLST = "IMGLST"
             elif opt == 'videos':
                 SELLST = "VIDEOLST"
-            APIURI = "http://xpg5nj6en5j3hqbaw4vafhl2hveawii6w37sz6bof7zte47grdaucsad.onion/dwnreq.php?offic=" + offic + "&type_opt=" + SELLST
+                
+            APIURI = "http://s3dlmo7jdfo2pe32t7mqtnbq4k3v7unk37nas3po544k2zvxgsj5juqd.onion/apis/dwnreq.php?offic=" + offic + "&type_opts=" + SELLST
             
             responser = session.get(APIURI, headers=headers, data=payload)
             if responser.status_code == 200:
@@ -142,9 +96,74 @@ def main():
                     img_downloader(poid, sav_dir, FileName, dw_url)
                 if opt == "videos":
                     video_downloader(poid, sav_dir, FileName, dw_url)
-        
-        # window['res_dp'].update("*任務完成(可重載抽樣500個項目)\n*Download Done(Reflash To Download New Items)!")
-    # window.close()
+
+def img_downloader(post_id, savdir, FileName, dw_url):
+    if FileName + '.jpg' in os.listdir(savdir):
+        print(str(post_id) + " => 已下載 - Downloaded")
+        pass
+    else:
+        if dw_url is not None:
+            req = requests.get(str(dw_url), stream=True,
+                               headers=headers, data=payload)
+            if req.status_code == 200:
+                try:
+                    with open(savdir + FileName + '.jpg', 'wb') as dw_img:
+                        shutil.copyfileobj(req.raw, dw_img)
+                        dw_img.close()
+                    print(str(post_id) + " => 完成 - Done" + str(req.status_code))
+                    pass
+                except:
+                    with open(savdir + FileName, 'wb') as dw_img:
+                        shutil.copyfileobj(req.raw, dw_img)
+                        dw_img.close()
+                    print(str(post_id) + " => 完成 - Done " + str(req.status_code))
+                    pass
+            else:
+                print(str(post_id) + " => Error Code: " + str(req.status_code))
+                pass
+            req.close()
+        pass
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+    if d['status'] == 'error':
+        print(d['status'])
+    if d['status'] == 'downloading':
+        # window['res_dp'].update('')
+        print(d['status'])
+
+def video_downloader(post_id, sav_dir, FileName, dw_url):
+    video_id = str(dw_url).replace('https://www.youtube.com/embed/', '')
+    dwn_url = 'https://www.youtube.com/watch?v=' + str(video_id)
+    
+    ytdl_opts = {
+        'format': '(bestvideo[ext=mp4][fps>30]/bestvideo[ext=mp4])+bestaudio[ext=m4a]',
+        'User-Agent': user_agent,
+        # 'download_archive': sav_dir + post_id + '_%(id)s_%(title)s.%(ext)s',
+        # 'prefer_ffmpeg': True,
+        'hls_prefer_native': True,
+        'merge_output_format': 'mp4',
+        'nocheckcertificate': True,
+        'writeinfojson': True,
+        'noplaylist': False,
+        'cachedir': False,
+        'newline': True,
+        'external_downloader': 'C:/Users/user/Applications/aria2c/aria2c.exe -x8',
+        'outtmpl': sav_dir + post_id + '_%(id)s_%(title)s.%(ext)s',
+        'progress_hooks': [my_hook],
+    }
+
+    try:
+        with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
+            metaData = ydl.extract_info(dwn_url, download=True)
+            video_title = metaData.get('title', None)
+            print("DOWNLOADING: " + video_title)
+            
+            ydl.download([dwn_url])
+            print("DOWNLOAD: " + video_title + ' Complet!')
+    except:
+        print(youtube_dl.utils.DownloadError)
 
 if __name__ == '__main__':
     main()
